5b2bca38add52097423a5ff33f04f39a96e1c245
Kevin Solorio <soloriok@gmail.com>
2019-08-18 16:00:14 -0500

    update build directory and compile contracts
    With the client requiring access to the contract *.json files
    we updated the config to place them there when they are recompiled

--- chapter-5/greeter/truffle-config.js
+++ chapter-5/greeter/truffle-config.js
@@ -25,6 +25,8 @@
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
+  contracts_build_directory: "./client/src/contracts",
+
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
